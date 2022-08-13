9144fdc563adec0547cec85ebafd48e286526f28
lestherl12 <106944199+lestherl12@users.noreply.github.com>
2022-08-13 10:10:06 -0600

    Update pitagoras.py

--- pitagoras.py
+++ pitagoras.py
@@ -1,6 +1,4 @@
 import math
-# hola bienvenido al programa de lesther
-# vamos a calcular la hipotenusa introduce sus lados o catetos
 ladoa=float(input("introduce el lado a:"))
 ladob=float(input("introduce el lado b:"))
 hipotenusa=math.sqrt((ladoa**2)+(ladob**2))
